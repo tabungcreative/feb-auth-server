91e5d00bb572739bc3281309ddf764124b1b83d1
tabungcreative <100331175+tabungcreative@users.noreply.github.com>
2022-09-27 09:14:41 +0700

    fitur auth server passport (#1)
    Co-authored-by: ahmdriffai <ahmmd.riffai@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
