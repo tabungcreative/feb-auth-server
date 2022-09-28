042eb6f69533832f094aed9f65eb0d220d533a06
ahmdriffai <ahmmd.riffai@gmail.com>
2022-09-28 15:05:54 +0700

    docker compose update and get logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -17,14 +19,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    foreach (User::find(4)->tokens as $token) {
+        $token->revoke();
+    }
     return view('welcome');
 });
 
 Auth::routes();
+Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth','super-admin'])->group(function() {
+Route::middleware(['auth', 'super-admin'])->group(function () {
     Route::resource('role', RoleController::class)->only('index', 'store');
     Route::resource('user', UserController::class)->only('index');
 });
