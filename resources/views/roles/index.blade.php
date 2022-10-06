eb2b9140c955c69456e22ca338f4600f91724cfb
ahmdriffai <ahmmd.riffai@gmail.com>
2022-10-06 11:23:54 +0700

    role create

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,38 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <a href="/home" class="btn btn-info">Kembali</a>
-            <button type="button" class="btn btn-primary my-3 shadow-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Tambah role
-            </button>
-
-            <div class="card border-0 shadow">
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card border-0 shadow">
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $item)    
                             <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Role</th>
-                                <th scope="col">Aksi</th>
+                                <th scope="row">#</th>
+                                <td>{{ $item->role }}</td>
+                                <td>@mdo</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($data as $item)    
-                                <tr>
-                                    <th scope="row">#</th>
-                                    <td>{{ $item->role }}</td>
-                                    <td>@mdo</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+    @canany(['dev','super-admin'])
+    <div class="col-md-6">
+        <div class="card border-0 shadow">
+            <div class="card-body">
+            <form action="{{ route('role.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label>Role</label>
+                    <input type="text" name="role" class="form-control" placeholder="Role">
+                </div>
+                <button type="submit" class="btn btn-primary"> Tambah</button>
+            </form>
+            </div>
+        </div>
+    </div>
+    @endcanany
 </div>
-@include('roles.modal')
 @endsection
